225b058185f4cadcbb3d58cbb45999018427f0d1
CyberSportsmen <alextinica007@gmail.com>
2025-04-04 18:37:34 +0300

    added sorting_tester class

--- parsing_binaries_example.cpp
+++ parsing_binaries_example.cpp
@@ -2,6 +2,65 @@
 #include <fstream>
 #include <string>
 
+enum SortingMethods
+{
+  STL,
+  RADIX10,
+  RADIX2TO16,
+  MERGE,
+  SHELL,
+  QUICK
+};
+
+class<T> Tester
+{
+  vector<T> v;
+
+public:
+  Tester() = default;
+  Tester(vector<T> v_clone)
+  {
+    v = v_clone;
+  }
+  Push_back(T val)
+  {
+    v.push_back(val);
+  }
+  Sort(method m)
+  {
+    switch (m)
+    {
+    case STL:
+      sort(v);
+      break;
+    case RADIX10:
+      sort(v);
+      break;
+    case RADIX2TO16:
+      sort(v);
+      break;
+    case MERGE:
+      sort(v);
+      break;
+    case SHELL:
+      sort(v);
+      break;
+    case QUICK:
+      sort(v);
+      break;
+    }
+  }
+};
+
+void Testcase(Tester t)
+{
+  for (auto method : SortingMethods)
+  {
+    // here we can place the chronometer logic
+    t.Sort(method);
+  }
+}
+
 int main(int argc, char *argv[])
 {
   std::string filename;
@@ -24,11 +83,10 @@ int main(int argc, char *argv[])
     return 1;
   }
   long long num;
+  Tester<long long> t;
   while (file.read(reinterpret_cast<char *>(&num), sizeof(long long)))
-  {
-    std::cout << num << ' '; // Print each number like a normal text file
-  }
-
+    t.push_back(num);
+  Testcase(t);
   file.close();
   return 0;
 }
